4c46db62a2aefa52b7b1175cb2d971819948ff14
benamedd <benseti@yahoo.fr>
2024-08-28 21:24:49 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const cacheName = 'potato-disease-cache-v1';
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll(assetsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
